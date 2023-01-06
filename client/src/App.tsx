3eb3fa7f0bbd95091d61cc73b94f5d53103b93fb
MaRcR11 <marcr.11@t-online.de>
2023-01-06 19:27:10 +0100

    first working crawl

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import crawler from "./crawler/crawler";
-import axios from "axios";
 function App() {
 
-    crawler.run(['https://crawlee.dev']).then(res => console.log(res))
 
     return (
         <div></div>
---
+++ server/crawler.js
@@ -0,0 +1,15 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+    try {
+        const browser = await puppeteer.launch();
+        const [page] = await browser.pages();
+        await page.goto( "https://selfservice.campus-dual.de/room/json?userid=5002080&hash=a0f51c26573dbf74502666d418252988&start=1648418400&end=1649023200&_=1648473223770", { waitUntil: 'networkidle0' });
+        const data = await page.evaluate(() => document.querySelector('pre').innerText);
+        console.log(data);
+        await browser.close();
+    } catch (err) {
+        console.error(err);
+    }
+
+})();
