01c479ab7f7aebb8e54749f1a8890bcad3743bb6
MaRcR11 <marcr.11@t-online.de>
2023-01-08 01:05:17 +0100

    deleting unnecessary code

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import {registerLicense} from '@syncfusion/ej2-base'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import "./styles/index.css";
+import { registerLicense } from "@syncfusion/ej2-base";
 
+registerLicense(
+  "ORg4AjUWIQA/Gnt2VVhkQlFacl1JXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxQdkRjXn5ccHRXRGBaVEM="
+);
 
-registerLicense('ORg4AjUWIQA/Gnt2VVhkQlFacl1JXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxQdkRjXn5ccHRXRGBaVEM=');
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- client/src/scheduleDataFormat.ts
+++ client/src/scheduleDataFormat.ts
@@ -1,15 +1,23 @@
 function scheduleDataFormat(
-  scheduleData: { start: number; end: number; title: string; remarks: string }[]
+  scheduleData: {
+    start: number;
+    end: number;
+    description: string;
+    remarks: string;
+    title: string;
+  }[]
 ): any {
   const formattedScheduleData: any = [];
-
+  const regexGroup = "Gruppe";
   scheduleData.map((e, i) => {
     formattedScheduleData.push({
       EndTime: new Date(e.end * 1000),
       StartTime: new Date(e.start * 1000),
-      Subject: e.title,
-      Location: e.remarks ? e.remarks : "BA Leipzig",
-      Owner: "Pter",
+      Subject: `${e.description + ` ` + `(${e.title})`}`,
+      Location:
+        e.remarks && !e.remarks.match(regexGroup)
+          ? e.remarks
+          : `BA Leipzig ${e.remarks ? `(${e.remarks})` : ""}`,
     });
   });
   return formattedScheduleData;
