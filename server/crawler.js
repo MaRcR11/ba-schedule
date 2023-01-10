464110d9c446bcdc53cb7fe4c66ee6d6cb55c5e8
Simon Nikolaidis <s-nikolaidis@web.de>
2023-01-10 23:29:55 +0100

    sini01# Bugfix Server überschreiben von data

--- server/crawler.js
+++ server/crawler.js
@@ -6,7 +6,7 @@ const crawler = async () => {
     const [page] = await browser.pages();
     await page.goto(
       "https://selfservice.campus-dual.de/room/json?userid=5002080&hash=a0f51c26573dbf74502666d418252988&start=1648418400&end=1649023200&_=1648473223770",
-      { waitUntil: "networkidle0", timeout: 0 }
+      { waitUntil: "networkidle0", timeout: 60000 }
     );
     const data = await page.evaluate(
       () => document.querySelector("pre").innerText
@@ -14,7 +14,7 @@ const crawler = async () => {
     await browser.close();
     return data;
   } catch (err) {
-    console.error(err);
+    throw new Error();
   }
 };
 
