bab87188be868fdf1a05de0222d69369a93bbbdd
Florian Schmidt <flo.schmidt10@gmail.com>
2023-01-24 20:34:23 +0100

    crawl using browserless http request

--- server/crawler.js
+++ server/crawler.js
@@ -1,21 +1,31 @@
-const puppeteer = require("puppeteer");
+const https = require('https');
+const fs = require('fs');
+
+var options = {
+  host: "selfservice.campus-dual.de",
+  path: "/room/json?userid=5002080&hash=a0f51c26573dbf74502666d418252988&start=1648418400&end=1649023200&_=1648473223770",
+  ca: fs.readFileSync(__dirname + '/campusdual-cert-chain.pem'),
+  json:true
+};
 
 const crawler = async () => {
   try {
-    const browser = await puppeteer.launch();
-    const [page] = await browser.pages();
-    await page.goto(
-      "https://selfservice.campus-dual.de/room/json?userid=5002080&hash=a0f51c26573dbf74502666d418252988&start=1648418400&end=1649023200&_=1648473223770",
-      { waitUntil: "networkidle0", timeout: 60000 }
-    );
-    const data = await page.evaluate(
-      () => document.querySelector("pre").innerText
-    );
-    await browser.close();
-    return data;
+    return new Promise((resolve) => {
+      https.get(options, function(res) {
+        res.setEncoding('utf8');
+        res_str = "";
+
+        res.on('data', (chunk) => {
+          res_str += chunk;
+        }).on('end', () => {
+          resolve(res_str)
+        });
+      });
+    });
+
   } catch (err) {
     throw new Error();
   }
-};
+}
 
 module.exports = crawler;
