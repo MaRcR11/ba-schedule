dc5cfdf3c3258219b536d1e118291b981e0848e7
MaRcR11 <marcr.11@t-online.de>
2023-02-01 21:42:45 +0100

    basic setup, but routes are not found?

--- server/src/controllers/schedule.controller.js
+++ server/src/controllers/schedule.controller.js
@@ -3,7 +3,8 @@ const path = require("path");
 
 async function getData(req, res, next) {
   try {
-    res.json(await scheduleService.getData(req));
+    const scheduleServiceGetData = await scheduleService.getData(req);
+    res.status(scheduleServiceGetData.status).json(scheduleServiceGetData.json);
   } catch (err) {
     console.error(`Error while getting index page`, err.message);
     next(err);
@@ -12,7 +13,8 @@ async function getData(req, res, next) {
 
 async function login(req, res, next) {
   try {
-    res.json(await scheduleService.login(req));
+    const scheduleServiceLogin = await scheduleService.login(req);
+    res.status(scheduleServiceLogin.status).json(scheduleServiceLogin.json);
   } catch (err) {
     console.error(`Error while login`, err.message);
     next(err);
