d5e215fe07dbb05272dcb320b0d621871d684111
Wegeleben <marc.wegeleben@softwareone.com>
2023-02-02 10:49:41 +0100

    test prettier workflow

--- client/src/helpers/loadingMessages.ts
+++ client/src/helpers/loadingMessages.ts
@@ -1,12 +1,7 @@
 import * as randomLoadingMessages from "../data/randomLoadingMessages.json";
 
 function getRandomLoadingMessage() {
-  return Object.values((randomLoadingMessages as any).default)[
-    Math.floor(
-      Math.random() *
-        Object.values((randomLoadingMessages as any).default).length
-    )
-  ];
+  return Object.values((randomLoadingMessages as any).default)[Math.floor(Math.random() * Object.values((randomLoadingMessages as any).default).length)];
 }
 
 export default getRandomLoadingMessage;
