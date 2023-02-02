3d7615ca512d50d7cef3bb471692b840483829b1
Wegeleben <marc.wegeleben@softwareone.com>
2023-02-02 10:54:37 +0100

    test prettier workflow

--- server/src/services/schedule.service.js
+++ server/src/services/schedule.service.js
@@ -11,9 +11,19 @@ let data;
 })();
 
 async function getData(req) {
-  if (!data) return { status: 502, json: "no data" };
+  if (!data) return { status: 502,
+
+
+    json: "no data" };
   const pwd = req.query.pwd;
-  const isPwdValid = await checkPwd(pwd);
+
+
+
+
+  const isPwdValid = await checkPwd(pwd
+
+
+  );
   if (!isPwdValid) return { status: 401, json: "not authorized" };
   return { status: 200, json: data };
 }
