2557a9d81529ff3f1b0ba401a0226f0b0befb2ab
MaRcR11 <91952791+MaRcR11@users.noreply.github.com>
2023-02-03 17:09:41 +0100

    Update setAppointmentColors.ts

--- client/src/helpers/setAppointmentColors.ts
+++ client/src/helpers/setAppointmentColors.ts
@@ -4,13 +4,9 @@ function setAppointmentColors(args: any, scheduleObj: any) {
   (subjects as any).default.map((e: any) => {
     let color = null;
 
-    if (args.data.Subject.includes("Fachenglisch")) {
-      console.log(args.data.Subject);
-    }
-    if (args.data.Location.includes("Prüfung")) {
-      console.log(args.data.Subject);
-      color = "#D2042D";
-    }
+
+    if (args.data.Location.includes("Prüfung")) color = "#D2042D";
+    
 
     args.data.Subject.includes(e.name)
       ? scheduleObj.currentView == "Agenda"
