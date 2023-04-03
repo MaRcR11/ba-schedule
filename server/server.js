b589620846e16586f5c124d471af5a366a564cf8
felikan <felix.herpich@softwareone.com>
2023-04-03 16:18:12 +0200

    rateLimiter is working now

--- server/server.js
+++ server/server.js
@@ -8,12 +8,12 @@ const dotenv = require("dotenv");
 const scheduleRouter = require("./src/routes/schedule.route");
 const rateLimiter = require("./src/middlewares/rateLimit.middleware");
 
+app.use(rateLimiter)
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../client/dist")));
 app.use(cors());
 app.use("/", scheduleRouter);
-app.use(rateLimiter);
 app.set("trust proxy", 1)
 dotenv.config();
 
