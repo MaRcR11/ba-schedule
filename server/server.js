a1f1ae2c260d727217145352745666ec9f9cade6
Wegeleben <marc.wegeleben@softwareone.com>
2023-04-04 10:20:53 +0200

    test workflow

--- server/server.js
+++ server/server.js
@@ -12,6 +12,20 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../client/dist")));
 app.use(cors());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.use("/", scheduleRouter);
 app.use(rateLimiter);
 app.set("trust proxy", 1)
