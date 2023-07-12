a9805831b7f3042203b05414cd98c86fa2f9b965
MaRcR11 <marcr.11@t-online.de>
2023-07-12 14:02:04 +0200

    update deps

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -15,6 +15,7 @@ function App() {
   const [storeUserIDRef, setStoreUserIDRef] = useState<string>("");
   const [scheduleData, setScheduleData] = useState<ScheduleData[]>([]);
 
+
   useEffect(() => {
     fireRedirect
       ? axios
