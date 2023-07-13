83abb6c2332429868561862886d712bda91369b3
MaRcR11 <MaRcR11@users.noreply.github.com>
2023-07-13 15:30:13 +0000

    Prettified Code!

--- client/src/components/SettingsOption.tsx
+++ client/src/components/SettingsOption.tsx
@@ -3,25 +3,25 @@ import ThemeToggle from "./ThemeToggle";
 import { SettingsOptionProps } from "../global/types";
 
 function SettingsOption(props: SettingsOptionProps) {
-    return (
-        <div className="field is-horizontal mb-3 is-block">
-            <div className="field-body is-block">
-                <div className="field is-narrow is-block">
-                    <div className="control">
-                        <div className="field is-grouped is-align-items-center is-justify-content-space-between">
+  return (
+    <div className="field is-horizontal mb-3 is-block">
+      <div className="field-body is-block">
+        <div className="field is-narrow is-block">
+          <div className="control">
+            <div className="field is-grouped is-align-items-center is-justify-content-space-between">
               <span className="option-text mr-6" style={{ fontSize: "0.85rem" }}>
                 {props.settingName}
               </span>
-                            <label className="switch">
-                                <ThemeToggle id={props.id} />
-                            </label>
-                        </div>
-                        <hr style={{ margin: "0" }} />
-                    </div>
-                </div>
+              <label className="switch">
+                <ThemeToggle id={props.id} />
+              </label>
             </div>
+            <hr style={{ margin: "0" }} />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 // if darkMode, all hr with backgroundColor: "#424242"
--- client/src/components/ThemeToggle.tsx
+++ client/src/components/ThemeToggle.tsx
@@ -1,28 +1,28 @@
 import React, { useState } from "react";
 
 interface Props {
-    id: string;
+  id: string;
 }
 
 function ThemeToggle(props: Props) {
-    const [isChecked, setIsChecked] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
 
-    const handleToggleChange = () => {
-        setIsChecked(!isChecked);
-    };
+  const handleToggleChange = () => {
+    setIsChecked(!isChecked);
+  };
 
-    return (
-        <div className="field">
-            <input
-                id={props.id}
-                type="checkbox"
-                checked={isChecked}
-                onChange={handleToggleChange}
-                className="is-rounded switch"
-            />
-            <label htmlFor={props.id} />
-        </div>
-    );
+  return (
+    <div className="field">
+      <input
+        id={props.id}
+        type="checkbox"
+        checked={isChecked}
+        onChange={handleToggleChange}
+        className="is-rounded switch"
+      />
+      <label htmlFor={props.id} />
+    </div>
+  );
 }
 
 export default ThemeToggle;
