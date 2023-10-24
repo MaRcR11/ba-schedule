e39849cc0e25fa62444981f7f74be6c952b2c48a
MaRcR11 <MaRcR11@users.noreply.github.com>
2023-10-24 13:33:46 +0000

    Prettified Code!

--- server/src/crawler/crawler.js
+++ server/src/crawler/crawler.js
@@ -5,7 +5,7 @@ const crawler = async (userID, userHash) => {
   const OPTIONS = {
     host: "selfservice.campus-dual.de",
     path: `/room/json?userid=${userID ? userID : 5002080}&hash=${
-      userHash ? userHash + "&end=" +  Math.floor(Date.now() / 100) : "a0f51c26573dbf74502666d418252988"
+      userHash ? userHash + "&end=" + Math.floor(Date.now() / 100) : "a0f51c26573dbf74502666d418252988"
     }`,
     ca: fs.readFileSync(__dirname + "/campusdual-cert-chain.pem"),
     json: true,
