57469cba39e691aa29e74ca0a1faefe33a23c2a6
Wegeleben <marc.wegeleben@softwareone.com>
2023-10-24 15:33:06 +0200

    fixed bug cause of different API approach of BA Dresden

--- server/src/crawler/crawler.js
+++ server/src/crawler/crawler.js
@@ -5,12 +5,11 @@ const crawler = async (userID, userHash) => {
   const OPTIONS = {
     host: "selfservice.campus-dual.de",
     path: `/room/json?userid=${userID ? userID : 5002080}&hash=${
-      userHash ? userHash : "a0f51c26573dbf74502666d418252988"
+      userHash ? userHash + "&end=" +  Math.floor(Date.now() / 100) : "a0f51c26573dbf74502666d418252988"
     }`,
     ca: fs.readFileSync(__dirname + "/campusdual-cert-chain.pem"),
     json: true,
   };
-
   let res_str;
   try {
     return new Promise((resolve) => {
