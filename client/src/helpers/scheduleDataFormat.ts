e77ffee27cb4707f009749e47e3f65da8a162777
MaRcR11 <marcr.11@t-online.de>
2023-11-11 23:27:39 +0100

    fixed..

--- client/src/helpers/scheduleDataFormat.ts
+++ client/src/helpers/scheduleDataFormat.ts
@@ -1,16 +1,13 @@
 import { FormattedScheduleData, ScheduleData } from "../global/types";
 
 const scheduleDataFormat = (scheduleData: ScheduleData[]): FormattedScheduleData[] =>
-  scheduleData.map((e) => ({
-    EndTime: new Date(e.end * 1000),
-    StartTime: new Date(e.start * 1000),
-    Subject: `${e.description.replace("VS", "")} (${e.title})`,
-    Location:
-      e.remarks && !e.remarks.includes("Gruppe") && !e.remarks.includes("Prüfung")
-        ? `${e.remarks} (${e.instructor})`
-        : `Berufsakademie ${e.remarks ? `(${e.remarks})` : ""} (${e.sroom ? e.sroom : "Raum unbekannt"}) (${
-            e.instructor
-          })`,
+  scheduleData.map(({ end, start, description, title, remarks, instructor, sroom }) => ({
+    EndTime: new Date(end * 1000),
+    StartTime: new Date(start * 1000),
+    Subject: description === title ? description.replace("VS", "") : `${description.replace("VS", "")} (${title})`,
+    Location: remarks && !remarks.includes("Gruppe") && !remarks.includes("Prüfung")
+      ? `${remarks} (${instructor})`
+      : `Berufsakademie ${remarks ? `(${remarks})` : ""} (${sroom ? sroom : "Raum unbekannt"}) (${instructor})`,
   }));
 
 export default scheduleDataFormat;
